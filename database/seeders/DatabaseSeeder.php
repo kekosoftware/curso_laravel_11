5bc070be30ef9e5a82a957002f096da0e4c31f37
Gustavo Ariel Coronel <ariel256@gmail.com>
2024-12-10 17:14:08 -0300

    Added the factories and relationships user->post

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Gus',
+            'email' => 'gus@mail.com',
+            'password' => bcrypt('gusabc123')
         ]);
+
+        User::factory(19)->hasPosts(4)->create();
+
     }
 }
