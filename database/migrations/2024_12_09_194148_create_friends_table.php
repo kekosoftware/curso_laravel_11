6e1de63e96b49ce4b3421ed85bfca149ce30d53b
Gustavo Ariel Coronel <ariel256@gmail.com>
2024-12-10 16:37:31 -0300

    Create the project and configurate the tables post and friends

---
+++ database/migrations/2024_12_09_194148_create_friends_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('friends', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('from_id'); // Define las columnas explícitamente
+            $table->unsignedBigInteger('to_id');   // Define las columnas explícitamente
+            $table->boolean('accepted')->default(false);
+            $table->timestamps();
+
+            // Define las claves foráneas después de las columnas
+            $table->foreign('from_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('to_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('friends');
+    }
+};
